a0f609125d9f5a0e6dd81209e028a2cc8f81468f
AtulPrajapat <atulprajapat77@gmail.com>
2021-08-05 17:22:36 +0530

    first commit

---
+++ src/main/java/in/atul/JenkinsExApplication.java
@@ -0,0 +1,13 @@
+package in.atul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExApplication.class, args);
+	}
+
+}
